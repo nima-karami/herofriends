96dfc2bfb7f5695c154349e8dd63c685d88e092f
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-05-18 19:41:18 -0400

    second commit

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Hello extends Component {
+    render() {
+        return(
+            <div className='Hello'>
+                Helloooo World
+            </div>
+        )
+    }
+};
+
+export default Hello; 
