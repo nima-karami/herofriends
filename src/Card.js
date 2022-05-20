82f4a833726035a00d0408dcb52e945814b690e8
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-05-19 23:35:49 -0400

    minor style changes

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ import './Card.css';
 const Card = (props) => {
     const {id, name, image} = props;
     return (
-        <div className = 'card pointer dim ba br3 ma2 shadow-2 dib'>
+        <div className = 'card pointer dim ba br3 ma1 shadow-2 dib'>
             <img alt = 'header' className = 'card-image tc br3 br--top' src={`${image}`} />
             <div className='card-footer'>
                 <h2 className='ma3'>{name}</h2>
