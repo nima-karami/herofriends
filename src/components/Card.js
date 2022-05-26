ff4e994ac3746040a1920526dfb06cd8e6c3f6fc
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-05-25 21:16:53 -0400

    adding icons

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Card.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faBrain, faHandFist, faBolt, faShieldHalved, faSwords  } from '@fortawesome/free-solid-svg-icons';
 
 const Card = (props) => {
     const {id, name, image, powerstats, publisher} = props;
@@ -10,7 +10,7 @@ const Card = (props) => {
             <img alt = 'header' className = 'card-image tc br3 br--top' src={`${image}`} />
             <div className='card-footer'>
                 <h2 className='ma3'>{name}</h2>
-                <p className='ma3'> INT: {powerstats.intelligence} STR: {powerstats.strength} SPD: {powerstats.speed}
+                <p className='ma3'> <FontAwesomeIcon icon={faBrain} />: {powerstats.intelligence} STR: {powerstats.strength} SPD: {powerstats.speed}
                  <br/> DUR: {powerstats.durability} PWR: {powerstats.power} CBT: {powerstats.combat}</p>
                 <p className='f7 ma3'>{publisher || "N/A"}</p>
             </div>
